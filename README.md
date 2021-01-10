53cca52bc458659517946c77473bfebe0eea58d5
Andrew <life.around.freaks@gmail.com>
2021-01-10 11:03:24 +0200

    Added some project info.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Calculator tool for crafting items in Barotrauma
 
+[Live version of the project is here](https://lifearoundfreaks.github.io/barotrauma-calc/).
+
 I do not claim any rights to images and data used. This is simply a convenience tool for the game called [Barotrauma](https://store.steampowered.com/app/602960/Barotrauma/) (v0.11.0.10).
--- src/BarotraumaCalc/Components/DefaultPage.js
+++ src/BarotraumaCalc/Components/DefaultPage.js
@@ -1,5 +1,9 @@
 export default function DefaultPage() {
     return <>
-        <h4>Home page with some useful info will be here</h4>
+        <h4>What I currently plan on adding:</h4>
+        <h6>Ability to choose a target destination. Buying prices will be from your starting destination and selling prices will be from the target one.</h6>
+        <h6>Main page calculator that will give you a summary of items which are profitable to buy for trade for crafting as well as the summary of most used ingredients in those crafts. All of that considering currently selected route.</h6>
+        <h6>Make items in  the "Used in" and "Scrapped from" blocks color coded to indicate which related crafts of the item are profitable.</h6>
+        <p>If you have found a bug or have a suggestion for me, you can <a href="https://github.com/lifearoundfreaks/barotrauma-calc/issues/new">start an issue</a>.</p>
     </>
 }
