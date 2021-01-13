8958b13398a7fba12cce0a30af5924330e4f0b4f
Andrew <life.around.freaks@gmail.com>
2021-01-13 10:09:33 +0200

    Forgot to remove the line from planned features.

--- src/BarotraumaCalc/Components/DefaultPage.js
+++ src/BarotraumaCalc/Components/DefaultPage.js
@@ -3,7 +3,6 @@ export default function DefaultPage(props) {
         {props.children}
         <h5 className="mb-4">What I currently plan on adding</h5>
         <p>At the moment it is assumed in the fabrication times that you have exactly the amount of skill needed. I need to add the ability to choose skill levels since they influence the crafting times.</p>
-        <p>Make items in the "Used in" and "Scrapped from" blocks color coded to indicate which related crafts of the item are profitable.</p>
         <p>Swap button for outosts.</p>
         <p>Main page item filtration.</p>
     </>
