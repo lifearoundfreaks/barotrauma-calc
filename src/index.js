961fd4595823678403736502fc9ca4f22e99f4f2
Andrew <life.around.freaks@gmail.com>
2021-01-13 16:27:59 +0200

    Removed unused stuff.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import initialState from './store'
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={initialState}>
-    <div className="App"><App/></div>
-  </Provider>,
+    <div className="App"><App/></div>,
   document.getElementById('root')
 );
