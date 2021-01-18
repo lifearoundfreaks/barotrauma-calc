5e7ac5f6b78bb0379f5be4264ee3fc5419023d8b
Andrew <life.around.freaks@gmail.com>
2021-01-18 10:25:50 +0200

    Added prices bug alert.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Container, Row, Col, Navbar } from 'react-bootstrap'
+import { useState } from 'react'
+import { Container, Row, Col, Navbar, Alert } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { SearchBar, ReputationInput, OutpostSelect, PageContents, BrandNavLogo, AdditionalFilters } from './BarotraumaCalc'
 import './App.css';
 
 export default function App() {
+
+  const [hideAlert, setHideAlert] = useState(false)
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
@@ -35,6 +38,11 @@ export default function App() {
       <Container className="pt-4 bg-white">
         <Row>
           <Col>
+            {hideAlert ? <></> : <Alert onClose={() => setHideAlert(true)} variant="danger" dismissible>
+              There is currently a bug in Barotrauma where buying prices would not update after moving to another location.
+              At the moment it can be solved by reloading you save file after entering an outpost. You can follow the progress of this
+              issue <a href="https://github.com/Regalis11/Barotrauma/issues/4803">here</a>.
+            </Alert>}
             <Route path="/" children={<PageContents />}></Route>
           </Col>
         </Row>
